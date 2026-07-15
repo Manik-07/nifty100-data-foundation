d150d2a36ae65251485f597cca5e8e1840986c14
Manik-07 <maniksoodan99@gmail.com>
2026-07-15 23:43:48 +0530

    Initial project setup

---
+++ main.py
@@ -0,0 +1,8 @@
+from rich import print
+
+def main():
+    print("[bold green]Nifty100 Data Foundation[/bold green]")
+    print("Environment setup completed successfully!")
+
+if __name__ == "__main__":
+    main()
